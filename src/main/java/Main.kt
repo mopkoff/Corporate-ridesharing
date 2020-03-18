7d95741dff88518e5966117c6920a83d44b8c637
dmitriy.tseyler <dmitriy.tseyler@jetbrains.com>
2020-03-18 21:43:55 +0300

    introduce person

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,32 +1,35 @@
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.*
+
 
 data class Point(val latitude: Float, val longitude: Float)
-data class Points(val passengers: Collection<Point>, val drivers: Collection<Point>)
+data class Participants(val passengers: Collection<Person>, val drivers: Collection<Person>)
+data class Person(val id: UUID, val finishPoint: Point)
 
 fun main() {
-    val points = readPoints()
-    for (passenger in points.passengers) {
-        val suggested = suggestDrivers(passenger, points.drivers)
-        for (point in suggested) {
-            println("${point.latitude}, ${point.longitude}")
+    val (passengers, drivers) = readPoints()
+    for (passenger in passengers) {
+        val suggestedDrivers = suggestDrivers(passenger, drivers)
+        for (driver in suggestedDrivers) {
+            println("${driver.finishPoint.latitude}, ${driver.finishPoint.longitude}")
         }
     }
 }
 
-fun suggestDrivers(passengerPoint: Point, driverPoints: Collection<Point>): Collection<Point> {
+fun suggestDrivers(passenger: Person, drivers: Collection<Person>): Collection<Person> {
     TODO("Implement me")
 }
 
-private fun readPoints(): Points {
+private fun readPoints(): Participants {
     val pathToResource = Paths.get(Point::class.java.getResource("latlons").toURI())
     val allPoints = Files.readAllLines(pathToResource).map { asPoint(it) }.shuffled()
-    val passengers = allPoints.slice(0..9)
-    val drivers = allPoints.slice(10..19)
-    return Points(passengers, drivers)
+    val passengers = allPoints.slice(0..9).map { Person(UUID.randomUUID(), it) }
+    val drivers = allPoints.slice(10..19).map { Person(UUID.randomUUID(), it) }
+    return Participants(passengers, drivers)
 }
 
-fun asPoint(it: String): Point {
+private fun asPoint(it: String): Point {
     val (lat, lon) = it.split(", ")
     return Point(lat.toFloat(), lon.toFloat())
 }
